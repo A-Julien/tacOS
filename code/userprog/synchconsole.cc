b3f6ed7e9a60643a41f51d24990050c50da2a3a0
hureau.olivier@gmail.com <hureau.olivier@gmail.com>
2020-01-07 22:03:22 +0100

    Copie des fichiers du PDF

---
+++ code/userprog/synchconsole.cc
@@ -0,0 +1,45 @@
+#ifdef CHANGED
+
+#include "copyright.h"
+#include "system.h"
+#include "synchconsole.h"
+#include "synch.h"
+
+static Semaphore *readAvail;
+static Semaphore *writeDone;
+
+static void ReadAvail(int arg) { readAvail->V(); }
+static void WriteDone(int arg) { writeDone->V(); }
+
+
+SynchConsole::SynchConsole(char *readFile, char *writeFile){
+	readAvail = new Semaphore("read avail", 0);
+	writeDone = new Semaphore("write done", 0);
+	console =  new Console (readFile, writeFile, ReadAvail, WriteDone, 0);
+}
+
+SynchConsole::~SynchConsole(){
+	delete console;
+	delete writeDone;
+	delete readAvail;
+}
+
+void SynchConsole::SynchPutChar(const char ch){
+	 console->PutChar (ch);
+     writeDone->P ();
+}
+
+char SynchConsole::SynchGetChar(){
+	 readAvail->P();
+	 return console->GetChar();
+}
+
+void SynchConsole::SynchPutString(const char s[]){
+// ...
+}
+
+void SynchConsole::SynchGetString(char *s, int n){
+// ...
+}
+
+#endif // CHANGED
