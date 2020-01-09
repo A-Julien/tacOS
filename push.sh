88e2f91e817afac11eaddf0524f5a133070b6799
Julien Alaimo <julien.alaimo@gmail.com>
2020-01-09 17:13:20 +0100

    Update push.sh

--- push.sh
+++ push.sh
@@ -6,6 +6,7 @@ setup_git() {
 merge_branch() {
     git config --add remote.origin.fetch +refs/heads/*:refs/remotes/origin/* || exit
     git fetch --all || exit
+    git stash || exit
     git checkout release || exit
     git merge --no-ff "$TRAVIS_COMMIT" || exit
     git push @github.com/">https://${GITHUB_TOKEN}@github.com/A-Julien/tacOS.git"
