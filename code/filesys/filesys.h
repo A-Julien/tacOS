236bcee41dae1f4932dfefe3cb3396953f2f53dd
Alaimo <julien.alaimo@gmail.com>
2020-01-20 14:30:13 +0100

    fix close error

--- code/filesys/filesys.h
+++ code/filesys/filesys.h
@@ -101,8 +101,8 @@ private:
 
     static OpenFile* open_files_table[];
 
-    static void close_open_files(OpenFile* open_files_table, int table_length=MAX_OPEN_FILE){
-        for (int i = 0; i < table_length; ++i) if(open_files_table[i]) delete open_files_table[i]
+    static void close_open_files(OpenFile** open_files_table, int table_length=MAX_OPEN_FILE){
+        for (int i = 0; i < table_length; ++i) if(open_files_table[i] != NULL) delete open_files_table[i];
     }
 
     static void init_open_files_table(){
